5bfb9e76fc232859ecb273fecc91edf66a70ca42
ivanpirat <ivan62196211@gmail.com>
2020-06-01 08:49:45 +0300

    1 binaries uploaded

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ class App extends Component {
               <th><h1 class="l" > contact us</h1> </th> </th>
          
        </table>
-       <img  class="fon" src="https://cdn.jsdelivr.net/gh/ivanpirat/react-g729fr@master/f2%20(2).jpg"/>
+       <img  class="fon" src="https://cdn.jsdelivr.net/gh/ivanpirat/react-g729fr@master/f2%20(2).jpg" 
+      
+       
+       />
        
       </div>
     );
