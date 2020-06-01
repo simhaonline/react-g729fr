77a5b24b6bc47cf2dda4adf7f4b0ec608619d1da
ivanpirat <ivan62196211@gmail.com>
2020-06-01 13:54:32 +0300

    3 files modified

--- index.js
+++ index.js
@@ -2,37 +2,6 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 import './index.css';
-
-class App1 extends Component {
-  render() {
-    return (
-      <div>
-       <div class="background"><img src="https://cdn.jsdelivr.net/gh/ivanpirat/react-g729fr@master/fon.jpg" /></div>
-        <footer>
-       <table>
-       <th class="ff"><h1 class="l" > your logo  </h1> </th>
-        <th class="q">  </th>
-          <th  > 
-          <th><h1 class="l" >  home</h1>      </th>
-          <th><h1 class="l"> about</h1>      </th>
-          <th><h1 class="l"> servicing</h1>  </th>
-            <th><h1 class="l" > portollo</h1>  </th>
-             <th><h1 class="l" > blog</h1>  </th>
-              <th><h1 class="l" > contact us</h1> </th> </th>
-         
-       </table>
-      </footer>
-       <h1 class="v" >Web development project</h1>
-       <h1 class="v1" >very sultable to support all web development projects</h1>
-       <table class="fonn" >
-       <th>
-      cvxvcxvcxvxcvxc
-       </th>
-       </table>
-      </div>
-    );
-  }
-}
 class App1 extends Component {
   render() {
     return (
@@ -49,16 +18,19 @@ class App1 extends Component {
            <th><h1 class="l" > contact us</h1> </th> 
            </th>
          
-       </table></tr>
+       </table>
+</tr>
 <tr> <img src="https://cdn.jsdelivr.net/gh/ivanpirat/react-g729fr@master/fon.jpg" /></tr>
-<tr>fdgfdgdgfd</tr>
-
-</table>
-
-
-
+ </table>
+  <table class="v">>
+    <tr > <h1  class="v1"  >Web development project</h1 ></tr> 
+      <tr >  <h1 class="v2"  >very sultable to support all web development projects</h1>  </tr> 
+    </table>
+  
  </div>
+ 
     )
  }
 }
+
 render(<App1 />, document.getElementById('root'));
