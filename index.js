03afea3aafd25eb0816c16624fbda9d076a0aed8
ivanpirat <ivan62196211@gmail.com>
2020-06-01 11:59:55 +0300

    2 files modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 
 import './index.css';
 
-class App extends Component {
+class App1 extends Component {
   render() {
     return (
       <div>
@@ -22,10 +22,43 @@ class App extends Component {
          
        </table>
       </footer>
-       
+       <h1 class="v" >Web development project</h1>
+       <h1 class="v1" >very sultable to support all web development projects</h1>
+       <table class="fonn" >
+       <th>
+      cvxvcxvcxvxcvxc
+       </th>
+       </table>
       </div>
     );
   }
 }
+class App1 extends Component {
+  render() {
+    return (
+       <div>
+<table class="fonn">
+<tr> <table class="text">
+       <th class="ff"><h1 class="l" > your logo  </h1> </th>
+          <th  class="ltetx" > 
+          <th><h1 class="l" >  home </h1>      </th>
+          <th><h1 class="l"> about</h1>      </th>
+          <th><h1 class="l"> servicing</h1>  </th>
+          <th><h1 class="l" > portollo</h1>  </th>
+           <th><h1 class="l" > blog</h1>  </th>
+           <th><h1 class="l" > contact us</h1> </th> 
+           </th>
+         
+       </table></tr>
+<tr> <img src="https://cdn.jsdelivr.net/gh/ivanpirat/react-g729fr@master/fon.jpg" /></tr>
+<tr>fdgfdgdgfd</tr>
 
-render(<App />, document.getElementById('root'));
+</table>
+
+
+
+ </div>
+    )
+ }
+}
+render(<App1 />, document.getElementById('root'));
